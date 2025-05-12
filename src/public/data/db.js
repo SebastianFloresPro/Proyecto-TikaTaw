357c7518797545f55c247dcc0dc5a136c616378b
Sebastian Flores <167713089+SebastianFloresPro@users.noreply.github.com>
2025-05-12 04:32:20 -0500

    Add files via upload

--- src/public/data/db.js
+++ src/public/data/db.js
@@ -1,19 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config(); // Esto carga las variables de entorno desde .env
+
 import mysql2 from 'mysql2';
 
+// Verifica que las variables se estén leyendo correctamente
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+
 const conexion = mysql2.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'dbtikitaw',
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 conexion.connect((err) => {
     if (err) {
-        console.error('Error al conectar base de datos');
+        console.error('Error al conectar a la base de datos:', err);
         return;
     }
-    console.log('Conectado a base de datos');
+    console.log('Conectado a la base de datos');
 });
 
 export default conexion;
