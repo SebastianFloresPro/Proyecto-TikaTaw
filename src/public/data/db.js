2c1c79841d2f93533bb25101afbac075d7b6498c
Sebastian Flores <167713089+SebastianFloresPro@users.noreply.github.com>
2025-05-12 06:21:16 -0500

    Update db.js

--- src/public/data/db.js
+++ src/public/data/db.js
@@ -1,15 +1,12 @@
-import dotenv from 'dotenv';
-dotenv.config(); // Carga las variables del .env
-
 import { Client } from 'pg';
 
-// Configura la conexión a PostgreSQL usando las variables de entorno correctamente
+// Configura la conexión a PostgreSQL directamente
 const client = new Client({
-  host: process.env.DB_HOST || 'dpg-d0gs6ak9c44c73943lcg-a.oregon-postgres.render.com', // Reemplaza el valor por tu DB_HOST real si no está en el .env
-  user: process.env.DB_USER || 'tikitaw_db_user',
-  password: process.env.DB_PASSWORD || 'D1Mw173Q5nJMKguEy14g1EE4qPLfrmQu',
-  database: process.env.DB_NAME || 'tikitaw_db',
-  port: process.env.DB_PORT || 5432,
+  host: 'dpg-d0gs6ak9c44c73943lcg-a.oregon-postgres.render.com', // Aquí va tu DB_HOST
+  user: 'tikitaw_db_user', // Aquí va tu DB_USER
+  password: 'D1Mw173Q5nJMKguEy14g1EE4qPLfrmQu', // Aquí va tu DB_PASSWORD
+  database: 'tikitaw_db', // Aquí va tu DB_NAME
+  port: 5432, // Puerto por defecto para PostgreSQL
   ssl: { rejectUnauthorized: false }, // Para conexiones seguras en Render
 });
 
